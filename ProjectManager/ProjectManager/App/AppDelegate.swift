1e9287c622f90e93d511e52f47bd5a9a83fd859a
Min Hyun <minnimida@gmail.com>
2023-10-09 21:42:07 +0900

    feat: Firebase 연결

--- ProjectManager/ProjectManager/App/AppDelegate.swift
+++ ProjectManager/ProjectManager/App/AppDelegate.swift
@@ -5,14 +5,12 @@
 //  Last modified by Max.
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
@@ -29,7 +27,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
