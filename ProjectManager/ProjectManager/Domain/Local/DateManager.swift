cb059efbc37d4e030bd0e22bacae529fe051629f
Min Hyun <minnimida@gmail.com>
2023-11-05 00:31:04 +0900

    chore: 불필요한 파일 삭제

--- ProjectManager/ProjectManager/Domain/Local/DateManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DateManager.swift
-//  ProjectManager
-//
-//  Created by Min Hyun on 2023/11/03.
-//
-
-import Foundation
--- ProjectManager/ProjectManager/Domain/Remote/FirebaseManager.swift
+++ ProjectManager/ProjectManager/Domain/Remote/FirebaseManager.swift
@@ -36,33 +36,7 @@ final class FirebaseManager {
             return Disposables.create()
         }
     }
-    
-//    func loadData<T: Decodable>(entityName: String, handler: @escaping (Result<[T], Error>) -> Void) {
-//        firebaseDB.child(entityName).getData { error, snapshot in
-//            if let error {
-//                handler(.failure(error))
-//                return
-//            }
-//
-//            guard let snapshot,
-//                  let data = snapshot.value as? [String: Any] else {
-//                handler(.failure(ProjectManagerError.dataNotFound))
-//                return
-//            }
-//
-//            let reducedData = data.reduce(into: []) { $0.append($1.value) }
-//
-//            do {
-//                let json = try JSONSerialization.data(withJSONObject: reducedData)
-//                let dto = try JSONDecoder().decode([T].self, from: json)
-//                handler(.success(dto))
-//
-//            } catch(let error) {
-//                handler(.failure(error))
-//            }
-//        }
-//    }
-    
+
     func changeData(entityName: String, id: String, values: [String: Any]) {
         firebaseDB.child(entityName).updateChildValues([id: values])
     }
